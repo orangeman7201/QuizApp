c12988fbdff29a7fe36dbc2d9cc28e5435951b06
unknown <aki.jazz1027@gmail.com>
2021-07-04 15:12:45 +0900

    JS課題④

---
+++ if.js
@@ -0,0 +1,9 @@
+// const startButton = document.getElementById('start-button');
+// const i = 0;
+// while (i < 10) {
+//     startButton.addEventListener('click', () => {
+//         i++;
+//         console.log(i)
+//     })
+// };
+// console.log('succece!!')
